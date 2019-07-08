ddf12ab10bc6172b90936e1c892ccfe98114bdca
Geron Sena <51683415+Wenyunity@users.noreply.github.com>
2019-07-08 02:20:17 -0400

    Iteration Added; comb() removed
    Removed comb() for a more space-efficient iteration.
    comb() would crash when given a high enough player count due to trying to store all combinations.

--- PenneyTournament.py
+++ PenneyTournament.py
@@ -17,22 +17,44 @@ def PenneyTournament(length=3, rounds=1000, players=2):
     worstWins = [rounds+1]*(2**length)
 
     ## Go through all combinations
-    bracket = list(comb(range(2**length), players))
-    for idx, val in enumerate(bracket):
+    moreGamesNeeded = True
+    ## Initalize player list
+    playerList = list(range(players-1, -1, -1))
+    while moreGamesNeeded:
         ## For each set of contestants
         winCount = [0]*players
         ## Play Game
         for i in range(rounds):
-            winner = Penney(val, length)
+            winner = Penney(playerList, length)
             winCount[winner] += 1
         ## Result counting
         for idxPlay, valWin in enumerate(winCount):
             ## Add wins
-            totalWins[val[idxPlay]] += valWin
-            if valWin < worstWins[val[idxPlay]]:
-                worstWins[val[idxPlay]] = valWin
-                worstGame[val[idxPlay]] = val
-        print("Game", val, "- Wins:", winCount)
+            totalWins[playerList[idxPlay]] += valWin
+            ## Worst Game Check
+            if valWin < worstWins[playerList[idxPlay]]:
+                worstWins[playerList[idxPlay]] = valWin
+                worstGame[playerList[idxPlay]] = list(playerList)
+        print("Game", playerList, "- Wins:", winCount)
+
+        ## Find next set
+        ## Add 1 to first contestant
+        playerList[0] += 1
+        for idx, val in enumerate(playerList):
+            ## If we've reached overflow
+            if idx + val >= 2**length:
+                ## If this is the final contestant, we're done
+                if idx + 1 == len(playerList):
+                    moreGamesNeeded = False
+                else: ## Add to the next contestant
+                    playerList[idx+1] += 1
+                    playerList[idx] = 0
+        ## Backfill any previous contestants to their next legal state.
+        for i in range(len(playerList)-2, -1, -1):
+            if playerList[i] < playerList[i+1]:
+                playerList[i] = playerList[i+1]+1
+    ## Summary Results
     print("Total wins: ", totalWins)
     print("Worst game: ", worstGame)
     print("Wins in worst game: ", worstWins)
+
